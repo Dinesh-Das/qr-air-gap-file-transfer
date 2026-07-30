8ff5ead967f6a8361b0b26834fbeb093118e991b
Dinesh-Das <dineshdas1016@gmail.com>
2026-07-30 22:25:44 +0530

    Initialize Vite React project

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: "es2022",
+    sourcemap: false,
+  },
+  server: {
+    host: "127.0.0.1",
+  },
+  preview: {
+    host: "127.0.0.1",
+  },
+  test: {
+    environment: "node",
+  },
+});
